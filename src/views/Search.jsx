8e5adaa6070355237b20dfba0c4b2388847ef47c
hakamfaza <hakamfaza@gmail.com>
2022-04-19 22:56:49 +0700

    add page search

---
+++ src/views/Search.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import Navbar from '../components/Navbar/Navbar';
+import CardResult from '../components/CardResult/CardResult';
+import styles from '../assets/styles/views/search.module.css';
+
+const Search = () => {
+  const { id } = useParams();
+
+  const [getRecipe, setGetRecipe] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/recipe?search=${id}`, {})
+      .then((response) => {
+        setGetRecipe(response.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      }, []);
+  });
+
+  return (
+    <>
+      <Navbar />
+      <div className={('container-fluid', styles.container)}>
+        <div className="row">
+          {getRecipe.map((e, i) => {
+            return (
+              <div className="col" key={i}>
+                <CardResult
+                  src={`${process.env.REACT_APP_API_URL}/image/${e.image}`}
+                  title={e.title}
+                  date={e.date}
+                  href={`/item/${e.id}`}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Search;
